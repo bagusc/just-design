3c19d777f28b3343b12dda928c5dcbd181a2f2b5
Bagus Cahyono <baguscyy@gmail.com>
2023-12-16 02:26:08 +0700

    fix: edit style🤭

--- app/src/main/java/com/example/just_design/DashboardActivity.java
+++ app/src/main/java/com/example/just_design/DashboardActivity.java
@@ -5,26 +5,46 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class DashboardActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        TextView greetingText = findViewById(R.id.greetingText);
+
+        // Mendapatkan waktu saat ini
+        Calendar calendar = Calendar.getInstance();
+        int hourOfDay = calendar.get(Calendar.HOUR_OF_DAY);
+
+        // Menentukan selamat pagi atau selamat malam berdasarkan waktu
+        if (hourOfDay >= 0 && hourOfDay < 12) {
+            greetingText.setText("Good Morning,");
+        } else if (hourOfDay >= 12 && hourOfDay < 18) {
+            greetingText.setText("Good Afternoon,");
+        } else if (hourOfDay >= 18 && hourOfDay < 0) {
+            greetingText.setText("Good Evening,");
+        }
     }
 
-    public void openInputDataActivity(View view) {
-        Intent intent = new Intent(this, InputDataActivity.class);
-        startActivity(intent);
-    }
 
-    public void openDisplayDataActivity(View view) {
-        Intent intent = new Intent(this, DisplayDataActivity.class);
-        startActivity(intent);
-    }
 
-    public void openManipulateDataActivity(View view) {
-        Intent intent = new Intent(this, ManipulateDataActivity.class);
-        startActivity(intent);
-    }
+//    public void openInputDataActivity(View view) {
+//        Intent intent = new Intent(this, InputDataActivity.class);
+//        startActivity(intent);
+//    }
+//
+//    public void openDisplayDataActivity(View view) {
+//        Intent intent = new Intent(this, DisplayDataActivity.class);
+//        startActivity(intent);
+//    }
+
+//    public void openManipulateDataActivity(View view) {
+//        Intent intent = new Intent(this, ManipulateDataActivity.class);
+//        startActivity(intent);
+//    }
 }
--- app/src/main/java/com/example/just_design/MainActivity.java
+++ app/src/main/java/com/example/just_design/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,12 +16,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextView myClickableText = findViewById(R.id.register);
+        Button loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginButton(v);
+            }
+        });
         myClickableText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 onRegClick(v);
             }
         });
+
+
+    }
+
+    // Metode yang akan dipanggil saat tombol diklik
+    public void loginButton(View view) {
+        // Navigasi ke activity lain
+        Intent intent = new Intent(this, DashboardActivity.class);
+        startActivity(intent);
     }
 
     // Metode yang akan dipanggil saat teks diklik
@@ -30,4 +47,6 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent( MainActivity.this, RegisterActivity.class);
         startActivity(intent);
     }
+
+
 }
