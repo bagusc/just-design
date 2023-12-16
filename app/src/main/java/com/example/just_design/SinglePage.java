901252c6336cf564a4400e708a7e869a8d5368cc
Bagus Cahyono <baguscyy@gmail.com>
2023-12-17 00:47:31 +0700

    feat: single page✅

---
+++ app/src/main/java/com/example/just_design/SinglePage.java
@@ -0,0 +1,14 @@
+package com.example.just_design;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SinglePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_page);
+    }
+}
